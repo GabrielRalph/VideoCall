9769046db7cfc4a7fc8f9c461dcd7c50ced415c5
GabrielRalph <gltralph@gmail.com>
2023-12-27 20:37:48 +1100

    ssd

--- src/Utilities/icons.js
+++ src/Utilities/icons.js
@@ -13,19 +13,24 @@ let Icons = {
 
 const DEFAULT_HEIGHT = 17.5;
 for (let name in Icons) {
-  let svgString = await (await fetch(`../Assets/i_${name}.svg`)).text();
-  let svg = SvgPlus.parseSVGString(svgString);
-  for (let e of svg.querySelectorAll("defs, style, script"))e.remove();
-  for (let e of svg.querySelectorAll("*")) {
-    if (e instanceof SVGGeometryElement) {
-      e.classList.add("i-fill");
-      e.classList.add(e.getAttribute("id"));
+  try {
+    let svgString = await (await fetch(`../Assets/i_${name}.svg`)).text();
+    let svg = SvgPlus.parseSVGString(svgString);
+    for (let e of svg.querySelectorAll("defs, style, script"))e.remove();
+    for (let e of svg.querySelectorAll("*")) {
+      if (e instanceof SVGGeometryElement) {
+        e.classList.add("i-fill");
+        e.classList.add(e.getAttribute("id"));
+      }
     }
+  
+    let h = parseFloat(svg.getAttribute("viewBox").split(" ")[3]);
+    svg.style.setProperty("--ws", Math.round(1000*h/DEFAULT_HEIGHT)/1000);
+  
+    Icons[name] = svg.outerHTML;
+
+  } catch(e) {
+
   }
-
-  let h = parseFloat(svg.getAttribute("viewBox").split(" ")[3]);
-  svg.style.setProperty("--ws", Math.round(1000*h/DEFAULT_HEIGHT)/1000);
-
-  Icons[name] = svg.outerHTML;
 }
 export {Icons}
