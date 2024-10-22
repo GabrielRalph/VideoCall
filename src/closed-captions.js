15e138133b9fc3cbed8dd6c41915917b3046580c
GabrielRalph <gltralph@gmail.com>
2024-10-22 23:19:28 +1100

    restart

--- src/closed-captions.js
+++ src/closed-captions.js
@@ -39,7 +39,7 @@ export function startRecognition(){
             console.error('Error occurred in recognition: ', event.error);
         };
         recognition.onend = () => {
-            console.log('Speech recognition service disconnected');
+            recognition.start();
         };
 
         recognition.start();
